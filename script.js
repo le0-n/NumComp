bdf56ecf0339f518a8dd0d3f58189338583d25f0
le0-n <farrux.farxodov2000@gmail.com>
2023-02-12 22:15:33 +0500

    NumComV1.1

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ let n2 = +prompt('Enter second number:  ')
 let n3 = +prompt('Enter third number:  ')
 
 if ((n1 > n2 && n1 > n3) && (n2 > n3)) {
-    alert(`${n1} is the biggest and ${n1} > ${n2} > ${n3}`)
+    alert(`${n1} is the biggest. \n${n1} > ${n2} > ${n3}`)
 } else if ((n1 > n2 && n1 > n3) && (n2 < n3)) {
-    alert(`${n1} is the biggest and ${n1} > ${n3} > ${n2}`)
+    alert(`${n1} is the biggest. \n${n1} > ${n3} > ${n2}`)
 } else if ((n2 > n1 && n2 > n3) && (n1 > n3)) {
-    alert(`${n2} is the biggest and ${n2} > ${n1} > ${n3}`)
+    alert(`${n2} is the biggest. \n${n2} > ${n1} > ${n3}`)
 } else if ((n2 > n1 && n2 > n3) && (n1 < n3)) {
-    alert(`${n2} is the biggest and ${n2} > ${n3} > ${n1}`)
+    alert(`${n2} is the biggest. \n${n2} > ${n3} > ${n1}`)
 } else if ((n3 > n1 && n3 > n2) && (n1 > n2)) {
-    alert(`${n3} is the biggest and ${n3} > ${n1} > ${n2}`)
+    alert(`${n3} is the biggest. \n${n3} > ${n1} > ${n2}`)
 } else if ((n3 > n1 && n3 > n2) && (n1 < n2)) {
-    alert(`${n3} is the biggest and ${n3} > ${n2} > ${n1}`)
+    alert(`${n3} is the biggest. \n${n3} > ${n2} > ${n1}`)
 }
